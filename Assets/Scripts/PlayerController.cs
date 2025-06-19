0a060b136231953dbe104cd3ca77b713bbd81baf
Michael Brooke <michaelbrooke93@gmail.com>
2025-06-19 16:28:36 +0200

    Rig up Idle and Running animations in the Player entity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private static readonly int MoveX = Animator.StringToHash("moveX");
+    private static readonly int MoveY = Animator.StringToHash("moveY");
+    
     [SerializeField] private float moveSpeed = 4f;
 
     // Reference to the generated code from the Input System
@@ -10,20 +13,37 @@ public class PlayerController : MonoBehaviour
     // Reference to current movement input direction from the Input System
     private Vector2 _movement;
     private Rigidbody2D _rb;
+    
+    private Animator _animator;
 
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void OnEnable() => _playerControls.Enable();
 
     private void Update() => PlayerInput();
 
-    private void FixedUpdate() => Move();
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
 
-    private void PlayerInput() => _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+    private void PlayerInput()
+    {
+        _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+        _animator.SetFloat(MoveX, _movement.x);
+        _animator.SetFloat(MoveY, _movement.y);
+    }
 
     private void Move() => _rb.MovePosition(_rb.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+
+    private void AdjustPlayerFacingDirection()
+    {
+        
+    }
 }
