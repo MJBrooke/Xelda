76dfae9dd4b23542007ca1b77d441cdf122517ea
Michael Brooke <michaelbrooke93@gmail.com>
2025-06-20 21:57:44 +0200

    Add initial Roaming State for Slime enemies

--- Assets/Scripts/Enemy AI.cs
+++ Assets/Scripts/Enemy AI.cs
@@ -1,16 +1,35 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private enum State
     {
-        
+        Roaming
+    }
+    
+    private State state;
+    private EnemyPathfinding _pathfinding;
+
+    private void Awake()
+    {
+        _pathfinding = GetComponent<EnemyPathfinding>();
+        state = State.Roaming;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(RoamingRoutine());
     }
+
+    private IEnumerator RoamingRoutine()
+    {
+        while (state == State.Roaming)
+        {
+            _pathfinding.SetDirection(GetRoamingPosition());
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private Vector2 GetRoamingPosition() => new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
 }
--- Assets/Scripts/Enemy Pathfinding.cs
+++ Assets/Scripts/Enemy Pathfinding.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class EnemyPathfinding : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 2f;
+    
+    private Vector2 _direction;
+    private Rigidbody2D _rb;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _direction = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        MoveSprite();
     }
+    
+    public void SetDirection(Vector2 direction) => _direction = direction;
+    
+    private void MoveSprite() => _rb.MovePosition(_rb.position + _direction * (moveSpeed * Time.fixedDeltaTime));
 }
