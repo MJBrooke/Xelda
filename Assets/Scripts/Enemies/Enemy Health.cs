4dbef40a29df9f49c63bc170395179958afa1297
Michael Brooke <michaelbrooke93@gmail.com>
2025-06-22 18:55:31 +0200

    Implement enemy damage and death from the Sword

--- Assets/Scripts/Enemies/Enemy Health.cs
+++ Assets/Scripts/Enemies/Enemy Health.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    [SerializeField] private int startingHealth = 3;
     
+    private int _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        DetectDeath();
+    }
+
+    private void DetectDeath()
+    {
+        if (_currentHealth <= 0) Destroy(gameObject);
+    }
 }
--- Assets/Scripts/Player/DamageSource.cs
+++ Assets/Scripts/Player/DamageSource.cs
@@ -2,11 +2,12 @@
 
 public class DamageSource : MonoBehaviour
 {
+    [SerializeField] private int damageAmount = 1;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<EnemyAI>())
-        {
-            Debug.Log("Hit!");
-        }
+        if (!other.gameObject.GetComponent<EnemyHealth>()) return;
+        
+        other.gameObject.GetComponent<EnemyHealth>().TakeDamage(damageAmount);
     }
 }
